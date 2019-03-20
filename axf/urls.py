feeb4ebc34b90521f10ca8b3ccd4cffa00618a0b
johnjgh <894016884@qq.com>
2019-03-20 11:17:23 +0800

    Initial commit

---
+++ axf/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+app_name = 'axf'
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('market/<str:categoryid>/<int:cid>/<int:sortid>/', views.market, name='market'),
+    path('cart/', views.cart, name='cart'),
+    path('changecart/<int:flag>/', views.changecart, name='changecart'),
+    path('mine/', views.mine, name='mine'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('checkuserid/', views.checkuserid, name = 'checkuserid'),
+    path('quit/', views.quit, name='quit'),
+]
